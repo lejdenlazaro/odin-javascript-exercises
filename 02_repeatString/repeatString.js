f58f64a77d1db5049ac3a0eee8731b2db2f59d83
Lejden Lazaro <lejdenlazaro@gmail.com>
2025-04-07 16:38:04 +0800

    Add repeat string function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, count) {
+  if (count < 0) return "ERROR";
+  let repeat = "";
+  for (let i = 0; i < count; i++) {
+    repeat += string;
+  }
+  return repeat;
 };
 
 // Do not edit below this line
