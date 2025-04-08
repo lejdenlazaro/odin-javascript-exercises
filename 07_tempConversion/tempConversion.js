e94e1b6dd6103df8b1d7ea928e50f57fea4903af
Lejden Lazaro <lejdenlazaro@gmail.com>
2025-04-09 04:35:44 +0800

    Enable all test cases for fahrenheit to celsius conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheitValue) {
+  return ((fahrenheitValue - 32) * 5) / 9;
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function () {};
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
