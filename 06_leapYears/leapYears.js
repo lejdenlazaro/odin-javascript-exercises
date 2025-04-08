b9bb69e04fd30b47c3ddc6960004d056d563f0e7
Lejden Lazaro <lejdenlazaro@gmail.com>
2025-04-09 04:30:10 +0800

    Add variable names to computations

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,7 +1,11 @@
 const leapYears = function (year) {
-  if (year % 4 === 0) {
-    if (year % 100 === 0) {
-      if (year % 400 === 0) return true;
+  const yearDivisibleByFour = year % 4 === 0;
+  const yearDivisibleByOneHundred = year % 100 === 0;
+  const yearDivisibleByFourHundred = year % 400 === 0;
+
+  if (yearDivisibleByFour) {
+    if (yearDivisibleByOneHundred) {
+      if (yearDivisibleByFourHundred) return true;
       return false;
     }
     return true;
