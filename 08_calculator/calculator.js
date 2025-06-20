d5b72ee3a402aca8d41183a8b455a709e4cc1642
Lejden Lazaro <lejdenlazaro@gmail.com>
2025-06-20 22:22:40 +0800

    Add solution to exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum,item)=>sum+item,0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product,item)=>product*item,1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num===0 || num===1) return 1;
+  let product = 1;
+  while(num){
+    product*=num;
+    num--;
+  }
+  return product;
 };
 
 // Do not edit below this line
